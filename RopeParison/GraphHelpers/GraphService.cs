4c568e00b194cdbdd2f45d69e916565c5f201c39
ropeparison <148985060+ropeparison@users.noreply.github.comcom>
2024-02-05 12:57:23 +0000

    Add graph service

---
+++ RopeParison/GraphHelpers/GraphService.cs
@@ -0,0 +1,33 @@
+﻿using RopeParison.Protocol;
+
+namespace RopeParison.GraphHelpers
+{
+    public interface IGraphService
+    {
+        Syncfusion.Blazor.Charts.ValueType GetAxisValueType(RopePropertyInformationType ropePropertyInformationType);
+    }
+
+    public class GraphService : IGraphService
+    {
+
+        //-------------------------------------------------------------------------
+
+        public Syncfusion.Blazor.Charts.ValueType GetAxisValueType(RopePropertyInformationType ropePropertyInformationType)
+        {
+            if (new Enum[] { RopePropertyInformationType.Double, RopePropertyInformationType.Int }.Contains(ropePropertyInformationType))
+            {
+                return Syncfusion.Blazor.Charts.ValueType.Double;
+            }
+            else if (new Enum[] { RopePropertyInformationType.String }.Contains(ropePropertyInformationType))
+            {
+                return Syncfusion.Blazor.Charts.ValueType.Category;
+            }
+            else
+            {
+                //Shouldn't get here. Use double as default
+                return Syncfusion.Blazor.Charts.ValueType.Double;
+            }
+        }
+
+    }
+}
