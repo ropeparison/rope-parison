f72aa87ebb3d1ee7fe5420f4a06920038f5eefa7
LukeBounds <113989216+LukeBounds@users.noreply.github.com>
2024-05-30 16:19:15 +0100

    Split IPassWordSecurityService and PasswordSecurityService into two files.
    Change security service namespaces.

---
+++ RopeParison.Security/Services/IPasswordSecurityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeParison.Security.Services
+{
+    public interface IPasswordSecurityService
+    {
+        bool CheckPassword_Verify(string proposedPassword);
+        bool CheckPassword_Edit(string proposedPassword);
+        bool CheckPassword_Delete(string proposedPassword);
+    }
+}
